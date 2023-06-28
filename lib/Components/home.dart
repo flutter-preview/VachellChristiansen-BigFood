cb14e2f659752cca50e74ee849f758156a448862
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2023-06-28 15:26:11 +0700

    grid in home

--- lib/Components/home.dart
+++ lib/Components/home.dart
@@ -20,7 +20,7 @@ class HomePage extends StatelessWidget {
   slivers: <Widget>[
     const SliverAppBar(
       pinned: true,
-      expandedHeight: 250.0,
+      expandedHeight: 100.0,
       flexibleSpace: FlexibleSpaceBar(
         title: Text('Find Your\nFavorite Food',
         style: TextStyle(
@@ -32,22 +32,44 @@ class HomePage extends StatelessWidget {
         
       ),
     ),
-    SliverGrid(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200.0,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        childAspectRatio: 4.0,
-      ),
-      delegate: SliverChildBuilderDelegate(
-        (BuildContext context, int index) {
-          return Container(
-            alignment: Alignment.center,
-            color: const Color.fromARGB(255,255, 238, 218),
-            child: Text('Grid Item $index'),
-          );
-        },
-        childCount: 20,
+  SliverPadding(
+      padding: const EdgeInsets.all(20),
+      sliver: SliverGrid.count(
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text("He'd have you all unravel at the"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text('Heed not the rabble'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text('Sound of screams but the'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text('Who scream'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text('Revolution is coming...'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: const Color.fromARGB(255, 255, 238, 218),
+            child: const Text('Revolution, they...'),
+          ),
+        ],
       ),
     ),
   ],
